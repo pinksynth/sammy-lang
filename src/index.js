947a51beff4a3b9bf5d4b1a78472039264a5f404
Sammy Taylor <sam.taylor@me.com>
2022-12-12 23:36:40 -0600

    Write as many files (tokens, ast, output) as possible even if subsequent steps fail

--- src/index.js
+++ src/index.js
@@ -5,28 +5,35 @@ const lex = require("./lexer")
 
 const fs = require("fs")
 
+const noop = () => {}
+
 const compile = ({ input, debug, jsGlobals, writeToFiles }) => {
   // eslint-disable-next-line no-unused-vars, no-undef
   const debugConsole = debug ? console : nullConsole
 
-  const tokens = lex(input)
-  // debugConsole.dir({ tokens }, { depth: null, maxArrayLength: null })
-
-  const ast = getAstFromTokens({ tokens, debug })
-  // debugConsole.dir({ ast }, { depth: null, maxArrayLength: null })
-
-  const js = jsCompile({ ast, debug, jsGlobals })
+  const write = writeToFiles
+    ? (filename, contents) => fs.writeFileSync(filename, contents)
+    : noop
 
   if (writeToFiles) {
     if (fs.existsSync("output")) {
       fs.rmSync("output", { recursive: true, force: true })
     }
     fs.mkdirSync("output")
-    fs.writeFileSync("output/input.sammy", input)
-    fs.writeFileSync("output/tokens.json", JSON.stringify(tokens))
-    fs.writeFileSync("output/ast.json", JSON.stringify(deleteParents(ast)))
-    fs.writeFileSync("output/jsOutput.js", js)
   }
+
+  write("output/input.sammy", input)
+
+  const tokens = lex(input)
+  // debugConsole.dir({ tokens }, { depth: null, maxArrayLength: null })
+  write("output/tokens.json", JSON.stringify(tokens))
+
+  const ast = getAstFromTokens({ tokens, debug })
+  // debugConsole.dir({ ast }, { depth: null, maxArrayLength: null })
+  write("output/ast.json", JSON.stringify(deleteParents(ast)))
+
+  const js = jsCompile({ ast, debug, jsGlobals })
+  write("output/jsOutput.js", js)
 
   return js
 }
